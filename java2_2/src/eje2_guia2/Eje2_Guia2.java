d2a1e9f2780a38bca3adce110fa89390c4843714
toby959 <134654437+toby959@users.noreply.github.com>
2023-08-11 19:53:28 -0300

    Add files via upload
    Guia 2 ejercicio 2

---
+++ java2_2/src/eje2_guia2/Eje2_Guia2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eje2_guia2;
+
+/**
+ *
+ * @author Moksys
+ */
+public class Eje2_Guia2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Temperatura celsAFahren = new Temperatura();
+        celsAFahren.setVisible(true);
+        celsAFahren.setLocationRelativeTo(null);
+        
+    }
+    
+}
