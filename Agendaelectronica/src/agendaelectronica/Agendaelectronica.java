88b6555f6741205dc14b249f790c606959b2a850
toby959 <134654437+toby959@users.noreply.github.com>
2023-08-12 23:59:29 -0300

    Add files via upload
    Agenda electrónica

---
+++ Agendaelectronica/src/agendaelectronica/Agendaelectronica.java
@@ -0,0 +1,22 @@
+
+package agendaelectronica;
+
+import agendaelectronica.igu.Principal;
+
+/**
+ *
+ * @author Usuario Package0>igu; logica; persistencia
+ */
+public class Agendaelectronica {
+
+    
+    public static void main(String[] args) {
+        Principal princ = new Principal(); //para mostrar x pantalla- 
+        princ.setVisible(true);            // sea visible x pantalla-
+        princ.setLocationRelativeTo(null); //posición pantalla-centro-
+    }
+    
+}
+
+
+
